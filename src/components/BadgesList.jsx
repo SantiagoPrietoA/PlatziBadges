72fbf239819c1509adc5c1894d7cbdc43d857eaf
Santiago Prieto <santiagoprietoacero@gmail.com>
2019-12-24 13:07:18 -0500

    Api request - badges list

--- src/components/BadgesList.jsx
+++ src/components/BadgesList.jsx
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 
+import { Link } from "react-router-dom";
+
 import './styles/BadgesList.css'
 
 class BadgesList extends Component {
 
     render() {
+        if( this.props.badges.length === 0) {
+            return(
+                <div>
+                    <h3>No badges were found</h3>
+                    <Link className="btn btn-primary" to="/badges/new">Create new badge</Link>
+                </div>
+            )
+        }
+
         return(
 
             <div className="BadgesList">
